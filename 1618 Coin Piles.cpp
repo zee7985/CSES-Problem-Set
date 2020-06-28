442a2fbd59a7e557f52d43389e6d93cdb30d9ebf
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-06-28 11:56:09 +0530

    Introductory Problems

---
+++ 1618 Coin Piles.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int a,b;
+        cin>>a>>b;
+        if((a+b)%3==0 && 2*min(a,b)>=max(a,b))
+        {
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
